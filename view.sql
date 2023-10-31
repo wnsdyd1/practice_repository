d96e2bde63d0f39b85ce8d085fa742e346ae0135
wnsdyd1 <asdf6226@naver.com>
2023-10-31 21:00:11 +0900

    [Feat] view연습

---
+++ view.sql
@@ -0,0 +1,35 @@
+#view
+CREATE VIEW v_memberbuy
+AS
+    SELECT B.mem_id, M.mem_name, B.prod_name, M.addr, 
+            CONCAT(M.phone1, M.phone2) '연락처' 
+       FROM buy B
+         INNER JOIN member M
+         ON B.mem_id = M.mem_id;
+
+SELECT * FROM v_memberbuy WHERE mem_name = '블랙핑크';
+
+CREATE VIEW v_viewtest1
+AS
+    SELECT B.mem_id 'Member ID', M.mem_name AS 'Member Name', 
+            B.prod_name "Product Name", 
+            CONCAT(M.phone1, M.phone2) AS "Office Phone" 
+       FROM buy B
+         INNER JOIN member M
+         ON B.mem_id = M.mem_id;
+         
+SELECT  DISTINCT `Member ID`, `Member Name` FROM v_viewtest1; 
+
+#view 수정
+ALTER VIEW v_viewtest1
+AS
+    SELECT B.mem_id '회원 아이디', M.mem_name AS '회원 이름', 
+            B.prod_name "제품 이름", 
+            CONCAT(M.phone1, M.phone2) AS "연락처" 
+       FROM buy B
+         INNER JOIN member M
+         ON B.mem_id = M.mem_id;
+         
+SELECT  DISTINCT `회원 아이디`, `회원 이름` FROM v_viewtest1; 
+
+DROP VIEW v_viewtest1;
